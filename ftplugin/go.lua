38ac4076c09cf294cb79b22e9242bb3f0fec19bc
ALizarazoTellez <andersonlizarazo27@gmail.com>
2024-03-10 15:45:54 -0500

    Add a Go filetype plugin

---
+++ ftplugin/go.lua
@@ -0,0 +1,8 @@
+-- Go Filetype Plugin.
+
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.gopls.setup({
+	capabilities = capabilities,
+})
