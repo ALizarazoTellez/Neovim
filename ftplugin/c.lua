42ad74198ebe188dcf4429f3294ac0cf8cec95fb
ALizarazoTellez <andersonlizarazo27@gmail.com>
2024-03-10 20:59:36 -0500

    Add C filetype plugin

---
+++ ftplugin/c.lua
@@ -0,0 +1,10 @@
+-- C Filetype Plugin.
+
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.clangd.setup({
+	capabilities = capabilities,
+})
+
+vim.cmd("LspStart")
