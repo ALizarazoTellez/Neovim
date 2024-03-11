606ba7b1713d960e485bff0849d8cb6afb839dc2
ALizarazoTellez <andersonlizarazo27@gmail.com>
2024-03-10 21:00:15 -0500

    Format files

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -1,15 +1,15 @@
 return {
-	'nvim-lualine/lualine.nvim',
+	"nvim-lualine/lualine.nvim",
 	dependencies = {
-		'nvim-tree/nvim-web-devicons'
+		"nvim-tree/nvim-web-devicons",
 	},
 
 	opts = {
 		options = {
 			icons_enabled = true,
-			theme = 'auto',
-			section_separators = { left = '', right = '' },
-			component_separators = { left = '', right = '' },
+			theme = "auto",
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
 			disabled_filetypes = {
 				statusline = {},
 				winbar = {},
@@ -21,27 +21,27 @@ return {
 				statusline = 1000,
 				tabline = 1000,
 				winbar = 1000,
-			}
+			},
 		},
 		sections = {
-			lualine_a = { 'mode' },
-			lualine_b = { 'branch', 'diff', 'diagnostics' },
-			lualine_c = { 'filename' },
-			lualine_x = { 'encoding', 'fileformat', 'filetype' },
-			lualine_y = { 'progress' },
-			lualine_z = { 'location' }
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
 		},
 		inactive_sections = {
 			lualine_a = {},
 			lualine_b = {},
-			lualine_c = { 'filename' },
-			lualine_x = { 'location' },
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
 			lualine_y = {},
-			lualine_z = {}
+			lualine_z = {},
 		},
 		tabline = {},
 		winbar = {},
 		inactive_winbar = {},
-		extensions = {}
+		extensions = {},
 	},
 }
