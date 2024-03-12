b3f2a0704525d29cf725d394d6e04ebdb6de89b5
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-03-11 21:11:23 -0500

    Add a TypeScript filetype plugin

---
+++ ftplugin/typescript.lua
@@ -0,0 +1,10 @@
+-- TypeScript Filetype Plugin.
+
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.tsserver.setup({
+	capabilities = capabilities,
+})
+
+vim.cmd("LspStart")
