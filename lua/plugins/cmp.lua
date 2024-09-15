67ae93b0c4b9639c50c219b4804f3fb88e3c89d2
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-09-15 17:07:04 -0500

    Change completion behavior

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,12 +11,6 @@ return {
 	config = function()
 		local cmp = require("cmp")
 
-		local has_words_before = function()
-			unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
-
 		cmp.setup({
 			sources = {
 				{ name = "nvim_lsp" },
@@ -32,22 +26,17 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(3),
 
 				["<C-e>"] = cmp.mapping.abort(),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						if #cmp.get_entries() == 1 then
-							cmp.confirm({ select = true })
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				["<Enter>"] = cmp.mapping({
+					i = function(fallback)
+						if cmp.visible() and cmp.get_active_entry() then
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
 						else
-							cmp.select_next_item()
+							fallback()
 						end
-					elseif has_words_before() then
-						cmp.complete()
-						if #cmp.get_entries() == 1 then
-							cmp.confirm({ select = true })
-						end
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+					end,
+					s = cmp.mapping.confirm({ select = true }),
+				}),
 			}),
 		})
 
