fd3aa79ce19a4cb8edacfbbd60970a37b2058e17
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-09-15 13:10:29 -0500

    Fix conflicting keymaps
    This keymap conflicts with LSP format.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,13 +7,13 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 
 	keys = {
-		{ "<Leader>f", desc = "Find files in Git" },
+		{ "<Leader>g", desc = "Find files in Git" },
 		{ "<Leader>s", desc = "Search content in files" },
 	},
 
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<Leader>f", builtin.git_files)
+		vim.keymap.set("n", "<Leader>g", builtin.git_files)
 		vim.keymap.set("n", "<Leader>s", builtin.live_grep)
 	end,
 }
