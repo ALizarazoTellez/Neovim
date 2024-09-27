c1d9469792d3af7cf45e779d6725f6850935bdfc
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-09-27 16:22:31 -0500

    Update ftplugin for Norg
    The `textwidth` option was removed due to incompatibility issues.
    
    Some mappings were also added.

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -1,8 +1,10 @@
 -- Neorg filetype plugin.
 
+-- Options.
 vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = "c"
-
-vim.opt_local.textwidth = 78
-
 vim.opt_local.fillchars = { eob = " " }
+
+-- Keymaps.
+vim.api.nvim_set_keymap("n", "<LocalLeader>i", "<Cmd>Neorg index<Enter>", { desc = "Go to Norg Index" })
+vim.api.nvim_set_keymap("n", "<Leader>f", "m'gg=G''", { desc = "Indent Norg File" })
