6e58ee06fbb069035a84f41b9afb85ea22118e9b
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-10-12 22:53:57 -0500

    Add basic LSP features, but autocompletion

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,21 @@
+return {
+	"neovim/nvim-lspconfig",
+
+	config = function()
+		-- Executes the callback function every time a
+		-- language server is attached to a buffer.
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
+			callback = function(event)
+				local opts = { buffer = event.buf }
+
+				vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, opts)
+				vim.keymap.set("n", "<Leader>r", vim.lsp.buf.rename, opts)
+
+				vim.keymap.set({ "n", "x" }, "<Leader>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
+			end,
+		})
+	end,
+}
