754ac8da84e1ff0d941bba586b1702788e852c3a
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-11-25 19:34:25 -0500

    Add filetype plugin for Python

---
+++ ftplugin/python.lua
@@ -0,0 +1,9 @@
+-- Python filetype plugin.
+
+-- Show tab characters as 6 spaces.
+vim.opt_local.tabstop = 6
+vim.opt_local.softtabstop = 0
+
+-- Use <Tab> to indent with 4 space characters.
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
