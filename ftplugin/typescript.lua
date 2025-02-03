1dc5e9d6be8d98f27b3b0367157107d9b61dd723
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2025-02-02 22:49:07 -0500

    Update filetype plugins for JS & TS

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -1,5 +1,13 @@
 -- TypeScript filetype plugin.
 
+-- Show tab characters as 3 spaces.
+vim.opt_local.tabstop = 3
+vim.opt_local.softtabstop = 0
+
+-- Use <Tab> to indent with 2 space characters.
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
+
 require("lspconfig").ts_ls.setup({})
 
 vim.cmd.LspStart()
